b9b941827b3bc57ee7b4e48c6af6775a644183f2
Raja Hasnain Anwar <rajahasnain570@gmail.com>
2017-07-09 11:33:37 +0500

    Day 01 Python

--- day_01_-_Hanoi_Tower.py
+++ day_01_-_Hanoi_Tower.py
@@ -1,3 +1,8 @@
-def hanoi(height, from='left', to='right', through='middle'):
+def hanoi(height, fro='left', to='right', through='middle'):
 	if height:
-		hanoi(height - 1)
+		hanoi(height - 1, fro, through, to )
+		print fro, ' => ', to
+		hanoi(height -1, through, to, fro)
+
+if __name__ == '__main__':
+	hanoi(5)
