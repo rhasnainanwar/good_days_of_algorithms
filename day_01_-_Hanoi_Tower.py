6af9e406b8cd35cb816a54e7a81e5d877960df17
nain570 <rajahasnain_570@yahoo.com>
2017-07-11 23:45:42 +0500

    remove extra files

--- day_01_-_Hanoi_Tower.py
+++
@@ -1,9 +0,0 @@
-import sys
-def hanoi(height, fro='left', to='right', through='middle'):
-        if height:
-                hanoi(height - 1, fro, through, to )
-                print ('%-7s => %s' % (fro, to))
-                hanoi(height -1, through, to, fro)
-
-if __name__ == '__main__':
-	hanoi( int(sys.argv[1]) )
