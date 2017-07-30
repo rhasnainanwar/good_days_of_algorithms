4ddbce053d9b417e84f6c077354db9080e319194
nain570 <rajahasnain_570@yahoo.com>
2017-07-31 01:25:29 +0500

    linked list in C, destroy not working

--- Day_16_-_LinkedLists.c
+++ Day_16_-_LinkedLists.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 /*-----  GLOBAL  -----*/
@@ -13,33 +12,128 @@ typedef struct list {
 } node;
 
 
-/*----- PROTOTYPES  -----*/
-//NOTE: all these function are declared after the main block
+/*-----  FUNCTIONS  -----*/
+//creates a node with int data and links it with the next node.
+//INPUT: a value for node, next node to link to
+//OUTPUT: pointer to the newly created node
+//EXPECTED ERROR: error while allocating memory to the node, memory restrictions
+node* create(int value, node* next){
 
-//creates a Linked List with an int data in head and a pointer to the next node.
-node* create(int, node*);
+    //allocating memory for new node
+    node* new_node = (node*)malloc(sizeof(node));
+
+    //exception
+    if(new_node == NULL){
+        fprintf(stderr, "ERROR: while creating node");
+        exit(1); //force terminate
+    }
+
+    //filling up
+    new_node->data = value;
+    new_node->next = next;
+
+    return new_node;
+}
 
 //adds a node to the beginning of an existing list (maybe empty or populated).
 //INPUTS: the value to be added to the node and the pointer to head of existing list
 //OUTPUTS: pointer to the head of new list, with a node prepended
-node* add(int, node*);
+//EXPECTED ERROR: it calls create() function, see its description
+node* add(int value, node* head){
+    //create a new node, which is to be added
+    node* new_node = create(value, head);
 
-//same as above, with a difference that the new node is added to the end of the list
-node* append(int, node*); //more like a push operation
+    //make the new node head
+    head = new_node;
+
+    return head;
+}
+
+//same description as add(), with a difference of node being added to the end
+node* append(int value, node* head){
+
+    node* pointer = head; //a pointer to head, which will move forward to point to all nodes 1-by-1
+
+    //keep moving until you reach the end
+    /*NOTE: I'm making use of the situation that the last node points to nothing!*/
+    while(pointer->next != NULL)
+        pointer = pointer->next;
+
+    //on reaching the end
+    node* new_node = create(value, NULL); //last one points to nothing
+    pointer->next = new_node; //attach to end
+
+    return head; //node added at the end, head remains the same
+}
 
 //tells if a node is present in the list
-int search(int, node*);
+//INPUT: a value to find, and the head of the list to find in
+//OUTPUT: 1 if value if found, 0 if not
+int search(int value, node* head){
+    node* pointer = head; //a pointer to point different nodes 1-by-1
 
-//removes a node from the front of the list
-node* delete(node*); //taken from Queue Data Structure; dequeue
+    while(pointer != NULL){ //until end is reached
+        if(pointer->data == value)
+            return 1;
+        pointer = pointer->next; //next one up for checking
+    }
+    return 0;
+}
 
-//removes the end node of the list
-node* pop(node*); //name taken from Stack Data Structure
+//removes a node from the front of the list, like dequeue in Data Structure QUEUE
+//INPUT: head of the list which is to be chopped
+//OUTPUT: new head of the list
+node* chop(node* head){
 
-//deletes the whole Linked List
-void* destroy(node*);
+    if(head == NULL)
+        return NULL;
 
-int main(){
+    node* new_head = head->next; //make the next node, the head
+    head->next = NULL; //old head points to nothing now
+    free(head); //free existing head, chop it!
 
+    return new_head;
+}
 
+//removes the end node of the list, like pop in Data Structure STACK
+//INPUT: head of the list
+//OUTPUT: head of the list
+node* pop(node* head){
+
+    if(head == NULL || head->next == NULL) //if list is empty or has only one node
+        return NULL;
+    node* pointer = head; //a pointer like other functions
+    node* prev = NULL; //previous node
+
+    while(pointer->next != NULL){
+        //current one becomes previous, a new one comes up
+        prev = pointer;
+        pointer = pointer->next;
+    }
+    prev->next = NULL; //making the second last pointer to nothing; aka last node
+    free(pointer); //emptying the last node
+
+    return head;//head unchanged
+}
+
+//deletes each node, i.e., whole Linked List
+//INPUT: head of the list to destroy
+//OUTPUT: NULL
+void destroy(node* head){
+    while(head != NULL)
+        head = pop(head);
+   head = NULL;
+}
+
+//prints the data in linked list. Name taken from Object class in Java
+//INPUT: head of the list
+//OUTPUT: NULL but prints the nodes on console
+void toString(node* head){
+    node* pointer = head;
+    printf("[ ");
+    while(pointer != NULL){ //until reached the end
+        printf("%d ", pointer->data); //display data in the node
+        pointer = pointer->next; //move to next node
+    }
+    printf("]\n");
 }
---
+++ Day_16_main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "Day_16_-_LinkedLists.c"
+
+int main(){
+    //starting with an empty node
+    node* head = NULL;
+    printf("Linked List: "); toString(head);
+
+    printf("\nAdding 7\n");
+    head = add(7, head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nAdding 1 in front\n");
+    head = add(1, head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nAdding 5 to the end\n");
+    head = append(5, head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nAdding 6\n");
+    head = add(6, head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nRemoving the front node\n");
+    head = chop(head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nRemoving the end node\n");
+    head = pop(head);
+    printf("Linked List: "); toString(head);
+
+    printf("\nSearching 6...\n");
+    if(search(6, head))
+        printf("FOUND!\n");
+    else
+        printf("NOT FOUND\n");
+
+    printf("\nSearching 1....\n");
+    if(search(1, head))
+        printf("FOUND!\n");
+    else
+        printf("NOT FOUND\n");
+
+    printf("\nDestroying...\n");
+    destroy(head);
+    printf("Linked List: "); toString(head);
+    return 0;
+}
